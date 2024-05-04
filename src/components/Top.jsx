d3594834f637382d6d9cde9b902ad412d2b754ab
Dust <03625@naver.com>
2024-05-04 11:36:21 +0900

    초안
    춘식이

---
+++ src/components/Top.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Top = () => {
+    return (
+        <div>
+            <img src="image/header.png" alt="" width="100%"/>
+        </div>
+    )
+}
+
+export default Top
---
+++ src/firebaseinit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBh4ZQX0D8YVsb-Q0ptWVp-ST-l9nOV3NU",
+  authDomain: "inha-1d2a1.firebaseapp.com",
+  projectId: "inha-1d2a1",
+  storageBucket: "inha-1d2a1.appspot.com",
+  messagingSenderId: "729916124835",
+  appId: "1:729916124835:web:c0ecd945b9ccb0da349916"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
